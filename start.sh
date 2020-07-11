d7ae52c616aaa3213026d95f0091a46fee2c48ff
gdtool <67850057+gdtool@users.noreply.github.com>
2020-07-11 15:41:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 #添加gd账号,设置密码
-adduser  gd -u 20001 -D -S -s /bin/bash -G root
-echo -e "${USERPWD}\n${USERPWD}" | passwd root
-echo -e "${USERPWD}\n${USERPWD}" | passwd gd
-chmod 4755 /bin/busybox
+#adduser  gd -u 20001 -D -S -s /bin/bash -G root
+#echo -e "${USERPWD}\n${USERPWD}" | passwd root
+#echo -e "${USERPWD}\n${USERPWD}" | passwd gd
+#chmod 4755 /bin/busybox
 node /gd-utils/server.js &
 #免登陆:/gd-utils/sa/shellinaboxd --no-beep -t  --service "/:root:root:/:/bin/bash" &
 /gd-utils/sa/shellinaboxd --no-beep -t -s "/:LOGIN"  
