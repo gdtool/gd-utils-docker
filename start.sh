026515c4ec81f73e05b13829b6a31396009ee7ec
gdtool <67850057+gdtool@users.noreply.github.com>
2020-08-10 13:16:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!bin/bash
+if [ ! -f "/gd-utils/sa/config.js" ];
+then
+ mv -f /gd-utils/config.js /gd-utils/sa/config.js
+ ln -sf /gd-utils/sa/config.js /gd-utils/config.js
+fi
+
+if [ ! -f "/gd-utils/sa/config.js" ];
+then
+ mv -f /gd-utils/gdurl.sqlite /gd-utils/sa/gdurl.sqlite 
+ ln -sf /gd-utils/sa/gdurl.sqlite /gd-utils/gdurl.sqlite
+fi
 #添加gd账号,设置密码
 adduser  gd -u 20001 -D -S -s /bin/bash -G root
 echo -e "${USERPWD}\n${USERPWD}" | passwd root
